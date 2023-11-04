525c8a2383531e06ccc8835bb4b1301b84f4681c
moe <pinktulip1027@gmail.com>
2023-11-05 01:04:53 +0900

    javascriptもじりきで作成中

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ stopwatchを作成
 
 htmlとCSSを自力で作成
 ・ボタンの配置幅を数字表示に合わせたいが分からない。
+
+javascriptも自力で作成中
---
+++ script/script.js
@@ -0,0 +1,16 @@
+const timer = document.getElementById('time');
+const startB = document.getElementById('start');
+const stopB = document.getElementById('stop');
+
+let start_t;
+let reset_t;
+let x;
+
+function displaytime(){
+   
+    console.log('dog');
+
+}
+
+displaytime();
+

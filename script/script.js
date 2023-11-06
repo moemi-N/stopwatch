ef9431f0c86373816eaf0d603f96c45b86195c68
moe <pinktulip1027@gmail.com>
2023-11-06 16:06:29 +0900

    関数に代入完了

--- script/script.js
+++ script/script.js
@@ -9,30 +9,48 @@ const stopB = document.getElementById('stop');
 let reset_t;
 let x;
 
+function string(mls){
+    // setInterval内が長くなったので経過msを表示する部分をここに記す。
+    const ms =mls % 1000;
+    // Math.floor()関数は与えられた数値以下の最大の整数を返す
+    const sec = Math.floor(mls / 1000) % 60;
+    const min = Math.floor(mls / (60*1000)) % 60;
+    // let Time = min + ":"+ sec + "."+ ms;
+
+    // 桁をそろえて表示
+    const fms = ms.toString().padStart(3,0);
+    const fsec = sec.toString().padStart(2,0);
+    const fmin = min.toString().padStart(2,0);
+    // let Time = fmin + ":"+ fsec + "."+ fms;
+    // let Time = `${fmin}:${fsec}.${fms}`;
+     // textContentで全てのhtml/cssの要素を取得できる⇔指定したIdに表示
+    // time.textContent = `${fmin}:${fsec}.${fms}`;
+
+    return `${fmin}:${fsec}.${fms}`;
+
+
+}
+
 // addEventlisnerは入力動作に対するイベント処理を行うメソッド
 startB.addEventListener('click',() =>{
-    
+        
     // console.log('dog');
         // const nowtime = Date.now;
         // 再定義不可、再代入可能→リセット機能させるために必要
         let start_t = Date.now();
+
     setInterval(() =>{
         // 毎設定秒後との時間を取得するから、再代入、再定義不可のconstを使用
         const now_t = Date.now();
         // const dis_t = now_t - start_t/1000;
         // ミリ秒についての表示なので余りでいい
         const dis_t = now_t - start_t;
-        const ms = dis_t % 1000;
-        // Math.floor()関数は与えられた数値以下の最大の整数を返す
-        const sec = Math.floor(dis_t/1000) % 60;
-        const min = Math.floor(dis_t/(60*1000)) % 60;
-        // textContentで全てのhtml/cssの要素を取得できる⇔指定したIdに表示
+       
+        timer.textContent = string(dis_t);
         
-        let Time = min + ":"+ sec + ":"+ ms;
-        time.textContent = Time;
     },10);
 
-    console.log('dog');
+    // console.log('dog');
 
 });
 

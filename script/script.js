a88957872c41d56107b3628814bed8fe1014c973
moe <pinktulip1027@gmail.com>
2023-11-06 17:02:34 +0900

    stopボタンの完了

--- script/script.js
+++ script/script.js
@@ -8,6 +8,9 @@ const stopB = document.getElementById('stop');
 let reset_t;
 let x;
 
+let state = 'start';
+let timerid;
+
 function string(mls){
     // setInterval内が長くなったので経過msを表示する部分をここに記す。
     const ms =mls % 1000;
@@ -27,19 +30,38 @@ function string(mls){
 
 // addEventlisnerは入力動作に対するイベント処理を行うメソッド
 startB.addEventListener('click',() =>{
-
+    if (state == 'start'){
         // 再定義不可、再代入可能→リセット機能させるために必要
         let start_t = Date.now();
 
-    setInterval(() =>{
-        // 毎設定秒後との時間を取得するから、再代入、再定義不可のconstを使用
-        const now_t = Date.now();
+        state ='stop';
+        startB.textContent = 'Stop';
+        // start.classList.add('stop');
+        startB.id = 'stop';
 
-        // ミリ秒についての表示なので余りでいい
-        const dis_t = now_t - start_t;
-        // textContentで全てのhtml/cssの要素を取得できる⇔指定したIdに表示
-        timer.textContent = string(dis_t);
-        
-    },10);
+        // 一定間隔で処理を実行する。
+        timerid = setInterval(() =>{
+            // 毎設定秒後との時間を取得するから、再代入、再定義不可のconstを使用
+            const now_t = Date.now();
+
+            // ミリ秒についての表示なので余りでいい
+            const dis_t = now_t - start_t;
+            // textContentで全てのhtml/cssの要素を取得できる⇔指定したIdに表示
+            timer.textContent = string(dis_t);
+            
+        },10);
+    }else{ 
+        state = 'stop';
+        // setIntervalでの処理を止める。
+        clearInterval(timerid);
+        startB.textContent = 'reStart';
+
+        startB.id = "start"
+    }
+
+
+     
+
+    
 });
 
